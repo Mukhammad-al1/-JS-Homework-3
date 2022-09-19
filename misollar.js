551bbf9d4e5b5b787b2b442d1808966710f3da5b
n.muhammadali17@gmail.com <nmuhammadali17@gmail.com>
2022-09-19 16:14:47 +0500

    Repository

---
+++ misollar.js
@@ -0,0 +1,92 @@
+// 1-misol
+// const result = (k, n) => {
+//   for (let i = 0; i <= n; i++) {
+//     console.log(`n=${i}, k=${k}`);
+//   }
+// };
+// result(10, 27);
+
+// 2-misol
+// const result = (a, b) => {
+//   let ok = 0;
+//   for (let i = a; i <= b; i++) {
+//     console.log(i);
+//     ok++;
+//   }
+// };
+// result(5, 14);
+
+// 3-misol
+// const result = (a, b) => {
+//   let ok = 0;
+//   for (let i = b - 1; i > a; i--) {
+//     console.log(i);
+//     ok++;
+//   }
+// };
+// result(5, 14);
+
+// 4 - misol
+// const result = (narx) => {
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(`${i} kg konfet: ${i * narx} so'm `);
+//   }
+// };
+// result(10000);
+
+// 5-misol
+// const result = (narx) => {
+//   for (let i = 0; i <= 1; i += 0.1) {
+//     console.log(`${i} kg konfet: ${i * narx} so'm `);
+//   }
+// };
+// result(10000);
+
+// 6-misol
+// const result = (narx) => {
+//   for (let i = 1; i <= 2; i += 0.2) {
+//     console.log(`${i} kg konfet: ${i * narx} so'm `);
+//   }
+// };
+// result(10000);
+
+// 7-misol
+// const result = (a, b) => {
+//   let x = 0;
+//   for (let i = a; i <= b; i++) {
+//     x += i;
+//   }
+//   console.log(`butun sonlarning yig'indisi: ${x}`);
+// };
+// result(1, 5);
+
+// 8-misol
+// const result = (a, b) => {
+//   let x = 1;
+//   for (let i = a; i <= b; i++) {
+//     x *= i;
+//   }
+//   console.log(`butun sonlarning ko'paytmasi: ${x}`);
+// };
+// result(1, 5);
+
+// 9-misol
+// const result = (a, b) => {
+//   let x = 1;
+//   for (let i = a; i <= b; i++) {
+//     x += i * i;
+//   }
+//   console.log(`kvadrat yig'indisi: ${x}`);
+// };
+// result(1, 5);
+
+// 10-misol
+// const result = (n) => {
+//   let x = 0;
+//   for (let i = 1; i <= n; i++) {
+//     console.log(i);
+//     x += 1 / i;
+//   }
+//   console.log("Natija:", x);
+// };
+// result(10);
